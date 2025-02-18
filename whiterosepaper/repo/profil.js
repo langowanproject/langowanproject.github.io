be523186d4a99f3f67a2e89b81f71f47d6c2bf48
langowanproject <langowanproject@outlook.co.id>
2025-02-19 07:58:13 +0800

    Add files via upload

--- whiterosepaper/repo/profil.js
+++ whiterosepaper/repo/profil.js
@@ -12,21 +12,21 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
         <div class="card-wrap">
             <div class="card left">
-                <div class="box bride-image"></div>
-                <h4 class="bride"></h4>
-                <p class="biodata-bride"></p>
+                <div class="box groom-image"></div>
+                <h4 class="groom"></h4>
+                <p>Anak <span class="groom-anak-ke"></span> dari Bapak <span class="ayah-groom"></span> dan Ibu <span class="ibu-groom"></span></p>
                 <div class="item-acara">
-                    <span><h5 class="icon">c</h5></span>
-                    <h5 class="alamat-bride"></h5>
+                    <span><h5 class="icon">c</h5></span><br>
+                    <h5 class="alamat-groom"></h5>
                 </div>
             </div>
             <div class="card right">
-                <div class="box groom-image"></div>
-                <h4 class="groom"></h4>
-                <p class="biodata-groom"></p>
+                <div class="box bride-image"></div>
+                <h4 class="bride"></h4>
+                <p>Anak <span class="bride-anak-ke"></span> dari Bapak <span class="ayah-bride"></span> dan Ibu <span class="ibu-bride"></span></p>
                 <div class="item-acara">
-                    <span><h5 class="icon">c</h5></span>
-                    <h5 class="alamat-groom"></h5>
+                    <span><h5 class="icon">c</h5></span><br>
+                    <h5 class="alamat-bride"></h5>
                 </div>
             </div>
         </div>
